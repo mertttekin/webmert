bdb4eb674fea42f3a98d9fb913d578d8442a761b
mertttekin <merttekin.bm@gmail.com>
2023-09-08 11:18:37 +0300

    added navbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Quicksand": [200, 300, 400, 500, 600, 700, 800],
+    },
+    display: "swap",
+    prefetch: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
